00c5beaaecbc1c0cfe4ef1c400e7a1467fa8ef15
SURAJ NAGAICH <suraj.nagaich_cs23@gla.ac.in>
2026-02-01 11:13:00 +0530

    Leetcode 950

---
+++ Week-3/Reveals card in increasing order.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] deckRevealedIncreasing(int[] deck) {
+        Arrays.sort(deck);
+        int n = deck.length;
+        Deque<Integer> queue = new ArrayDeque<>();
+        for(int i=n-1; i>=0; i--) {
+            if(!queue.isEmpty()) {
+                int x = queue.removeLast();
+                queue.addFirst(x);
+            }
+            queue.addFirst(deck[i]);
+        }
+        int[] ans = new int[n];
+        int i=0;
+        for(int x : queue) {
+            ans[i++] = x;
+        }
+        return ans;
+    }
+}
